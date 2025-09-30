e5e0b376e19c0a8782c5b3359b06d7b5eb5ad7a2
Davis Rapp <davisrapp@yahoo.com>
2025-09-30 23:32:28 +0000

    Fix: Apply geolocation filtering before saving file

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -25,15 +25,15 @@ def go(args):
     # Drop outliers
     min_price = args.min_price
     max_price = args.max_price
-    idx = df['price'].between(min_price, max_price)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
-
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
     # Save the cleaned file
     df.to_csv('clean_sample.csv',index=False)
-    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    df = df[idx].copy()
+    
     # log the new data.
     artifact = wandb.Artifact(
      args.output_artifact,
